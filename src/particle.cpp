c887603a63a5b3be01ed586e0d8b464197ad2c8b
vmaguiar <vmaguiar@hotmail.com>
2025-06-18 23:55:21 -0300

    refactor: changes particle members names

--- src/particle.cpp
+++ src/particle.cpp
@@ -4,97 +4,99 @@
 
 Particle::Particle(sf::Vector2f startPosition, sf::Vector2f startVelocity, sf::Color startColor, float startSize, float lifeTime,
                    sf::Vector2f startAcceleration, float rotationSpeed, float scaleRate,
-                   float alphaDecayRate): m_particleVelocity(startVelocity),
-                                          m_particleAcceleration(startAcceleration),
-                                          m_particleCurrentRotation(0.0f),
-                                          m_particleRotationSpeed(rotationSpeed),
-                                          m_particleInitialSize(startSize),
-                                          m_particleCurrentSize(startSize),
-                                          m_particleScaleRate(scaleRate),
-                                          m_particleInitialColor(startColor),
-                                          m_particleCurrentAlpha(static_cast<float>(startColor.a)),
-                                          m_particleDecayRate(alphaDecayRate),
+                   float alphaDecayRate): m_velocity(startVelocity),
+                                          m_acceleration(startAcceleration),
+                                          m_currentRotation(0.0f),
+                                          m_rotationSpeed(rotationSpeed),
+                                          m_initialSize(startSize),
+                                          m_currentSize(startSize),
+                                          m_scaleRate(scaleRate),
+                                          m_initialColor(startColor),
+                                          m_currentAlpha(static_cast<float>(startColor.a)),
+                                          m_decayRate(alphaDecayRate),
                                           m_spawnTime(std::chrono::high_resolution_clock::now()),
-                                          m_particleLifetime(lifeTime) {
-    m_particleShape.setSize({m_particleInitialSize, m_particleInitialSize});
-    m_particleShape.setFillColor(m_particleInitialColor);
-    m_particleShape.setOrigin({m_particleInitialSize * 0.5f, m_particleInitialSize * 0.5f}); // origin in the middle of the square
-    m_particleShape.setPosition(startPosition);
+                                          m_lifetime(lifeTime) {
+    m_shape.setSize({m_initialSize, m_initialSize});
+    m_shape.setFillColor(m_initialColor);
+    m_shape.setOrigin({m_initialSize * 0.5f, m_initialSize * 0.5f}); // origin in the middle of the square
+    m_shape.setPosition(startPosition);
 }
 
-void Particle::updateParticle(float dt) {
+void Particle::update(float dt) {
     auto now = std::chrono::high_resolution_clock::now();
     float elapsedTime = std::chrono::duration_cast<std::chrono::duration<float> >(now - m_spawnTime).count();
 
     // if particle is already dead dont update
-    if (elapsedTime >= m_particleLifetime && m_particleCurrentAlpha <= 0.0f) {
+    if (elapsedTime >= m_lifetime && m_currentAlpha <= 0.0f) {
         return;
     }
 
     // 1. Apply acceleration for smooth stop and steam effect
-    m_particleVelocity += m_particleAcceleration * dt;
-    m_particleShape.move(dt * m_particleVelocity);
+    m_velocity += m_acceleration * dt;
+    m_shape.move(dt * m_velocity);
 
     // 2. Apply rotation
-    if (m_particleRotationSpeed != 0.0f) {
-        m_particleCurrentRotation += m_particleRotationSpeed * dt;
+    if (m_rotationSpeed != 0.0f) {
+        m_currentRotation += m_rotationSpeed * dt;
 
-        if (m_particleCurrentRotation >= 360.0f) {
-            m_particleCurrentRotation -= 360.0f;
+        if (m_currentRotation >= 360.0f) {
+            m_currentRotation -= 360.0f;
         }
-        else if (m_particleCurrentRotation < 0.0f) {
-            m_particleCurrentRotation += 360.0f;
+        else if (m_currentRotation < 0.0f) {
+            m_currentRotation += 360.0f;
         }
-        m_particleShape.setRotation(sf::degrees(m_particleRotationSpeed));
+        m_shape.setRotation(sf::degrees(m_rotationSpeed));
     }
 
     // 3. Apply increasing size
-    if (m_particleScaleRate != 0.0f) {
-        m_particleCurrentSize += m_particleScaleRate * dt;
-        m_particleCurrentSize = std::max(0.0f, m_particleCurrentSize);
+    if (m_scaleRate != 0.0f) {
+        m_currentSize += m_scaleRate * dt;
+        m_currentSize = std::max(0.0f, m_currentSize);
 
-        m_particleShape.setSize({m_particleCurrentSize, m_particleCurrentSize});
-        m_particleShape.setOrigin({m_particleCurrentSize * 0.5f, m_particleCurrentSize * 0.5f});
+        m_shape.setSize({m_currentSize, m_currentSize});
+        m_shape.setOrigin({m_currentSize * 0.5f, m_currentSize * 0.5f});
     }
 
     // 4. Apply decreasing alpha
-    if (m_particleCurrentAlpha != 0.0f) {
-        m_particleCurrentAlpha -= m_particleDecayRate * dt;
+    if (m_currentAlpha != 0.0f) {
+        m_currentAlpha -= m_decayRate * dt;
 
-        m_particleCurrentAlpha = std::max(0.0f, m_particleCurrentAlpha);
-        m_particleCurrentAlpha = std::min(255.0f, m_particleCurrentAlpha);
+        m_currentAlpha = std::max(0.0f, m_currentAlpha);
+        m_currentAlpha = std::min(255.0f, m_currentAlpha);
 
-        sf::Color currentColor = m_particleShape.getFillColor();
-        currentColor.a = static_cast<uint8_t>(m_particleCurrentAlpha);
-        m_particleShape.setFillColor(currentColor);
+        sf::Color currentColor = m_shape.getFillColor();
+        currentColor.a = static_cast<uint8_t>(m_currentAlpha);
+        m_shape.setFillColor(currentColor);
     }
 }
 
-void Particle::drawParticle(sf::RenderWindow &window) const {
-    if (m_particleShape.getFillColor().a > 0) {
-        window.draw(m_particleShape);
+
+void Particle::draw(sf::RenderWindow &window) const {
+    if (m_shape.getFillColor().a > 0) {
+        window.draw(m_shape);
     }
 }
 
-bool Particle::isParticleDead() const {
+
+bool Particle::isDead() const {
     auto now = std::chrono::high_resolution_clock::now();
     float elapsedTime = std::chrono::duration_cast<std::chrono::duration<float> >(now - m_spawnTime).count();
-    return elapsedTime >= m_particleLifetime || m_particleCurrentAlpha <= 0.0f;
+    return elapsedTime >= m_lifetime || m_currentAlpha <= 0.0f;
 }
 
 
 sf::Vector2f Particle::getPosition() const {
-    return m_particleShape.getPosition();
+    return m_shape.getPosition();
 }
 
 sf::Vector2f Particle::getVelocity() const {
-    return m_particleVelocity;
+    return m_velocity;
 }
 
 void Particle::setPosition(sf::Vector2f newPosition) {
-    m_particleShape.setPosition(newPosition);
+    m_shape.setPosition(newPosition);
 }
 
 void Particle::setVelocity(sf::Vector2f startVelocity) {
-    m_particleVelocity = startVelocity;
+    m_velocity = startVelocity;
 }
--- src/particle.hpp
+++ src/particle.hpp
@@ -4,34 +4,34 @@
 #include "SFML/System/Vector2.hpp"
 
 class Particle {
-    sf::RectangleShape m_particleShape;
-    sf::Vector2f m_particleVelocity;
-    sf::Vector2f m_particleAcceleration;
+    sf::RectangleShape m_shape;
+    sf::Vector2f m_velocity;
+    sf::Vector2f m_acceleration;
 
-    float m_particleCurrentRotation; // degrees
-    float m_particleRotationSpeed; // degrees per seconds
+    float m_currentRotation; // degrees
+    float m_rotationSpeed; // degrees per seconds
 
-    float m_particleInitialSize;
-    float m_particleCurrentSize;
-    float m_particleScaleRate; // pixels per seconds ou %/sec
+    float m_initialSize;
+    float m_currentSize;
+    float m_scaleRate; // pixels per seconds ou %/sec
 
-    sf::Color m_particleInitialColor;
-    float m_particleCurrentAlpha; // (0-255) empty - full
-    float m_particleDecayRate; // alpha per seconds
+    sf::Color m_initialColor;
+    float m_currentAlpha; // (0-255) empty - full
+    float m_decayRate; // alpha per seconds
 
     std::chrono::high_resolution_clock::time_point m_spawnTime;
-    float m_particleLifetime; // total in seconds
+    float m_lifetime; // total in seconds
 
 public:
     Particle(sf::Vector2f startPosition, sf::Vector2f startVelocity, sf::Color startColor, float startSize, float lifeTime,
              sf::Vector2f startAcceleration = sf::Vector2f(0.0f, 0.0f), float rotationSpeed = 0.0f, float scaleRate = 0.0f,
              float alphaDecayRate = 0.0f);
 
-    void updateParticle(float dt);
+    void update(float dt);
 
-    void drawParticle(sf::RenderWindow &window) const;
+    void draw(sf::RenderWindow &window) const;
 
-    bool isParticleDead() const;
+    bool isDead() const;
 
     sf::Vector2f getPosition() const;
 
